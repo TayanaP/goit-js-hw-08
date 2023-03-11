aca6fea4de3d93e1c4175dea9025bc24285062f8
Tetiana Pesotska <verhulev81@gmail.com>
2023-03-11 02:25:03 +0200

    change js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+console.log(SimpleLightbox);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import templateFunction from '../templates';
+console.log(templateFunction);
+
+const galleryEl = document.querySelector('.gallery');
+const pictureCart = createPictureCart(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', pictureCart);
+
+function createPictureCart(galleryItems) {
+  return galleryItems.map(templateFunction).join('');
+}
+console.log(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
